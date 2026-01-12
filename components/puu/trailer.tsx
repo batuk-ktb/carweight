df6e40a7034fd7d0f62935d45668825a68f1aa80
batuk <45119932+batuk-ktb@users.noreply.github.com>
2026-01-12 09:20:00 +0800

    image

--- components/puu/trailer.tsx
+++ components/puu/trailer.tsx
@@ -1,20 +1,41 @@
-import CameraPlayer from "../CameraPlayer"
-const Trailer = ({ data }: any) => {
+type TrailerProps = {
+  data: {
+    cam1?: string
+    cam2?: string
+  }
+}
+
+const Trailer = ({ data }: TrailerProps) => {
   return (
-    <div className="flex flex-col justify-center items-center relative">
-      {/* chirguul1 */}
-      <div className="w-ful items-center absolute -top-3 left-0">
-        {/*  video here */}
-        <div className="w-[200px] h-[120px]">
-          <CameraPlayer src={'rtsp://admin:admin123@172.16.92.70:554/cam/realmonitor?channel=1&subtype=0'} />
+    <div className="flex flex-col justify-center items-center relative gap-2">
+
+      {/* Баруун камер */}
+      <div className="flex flex-col items-center">
+        <div className="w-[200px] h-[120px] border bg-black overflow-hidden">
+          <img
+            src={data?.cam1 || "/no-signal.jpg"}
+            alt="Right camera"
+            className="w-full h-full object-cover"
+          />
         </div>
-        <p>Баруун тал:{data.cam1}</p>
+        <p>Баруун тал</p>
       </div>
-      <div className="w-[200px] h-[80px] bg-blue-500">
+
+      {/* Trailer body */}
+      <div className="w-[200px] h-[80px] bg-blue-500"></div>
+
+      {/* Зүүн камер */}
+      <div className="flex flex-col items-center">
+        <div className="w-[200px] h-[120px] border bg-black overflow-hidden">
+          <img
+            src={data?.cam2 || "/no-signal.jpg"}
+            alt="Left camera"
+            className="w-full h-full object-cover"
+          />
+        </div>
+        <p>Зүүн тал</p>
       </div>
-      <div className="w-ful items-center absolute -bottom-3 right-0">
-        <p>Зүүн тал:{data.cam2}</p>
-      </div>
+
     </div>
   )
 }
